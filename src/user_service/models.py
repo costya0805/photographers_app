50a9a9d54adab49cac154af9954191103d0bb5c4
ipuzanov <ipuzanov@ussc.ru>
2021-11-21 20:24:54 +0500

    Добавил таблицу пользователей

--- src/user_service/models.py
+++ src/user_service/models.py
@@ -1,17 +1,56 @@
-from sqlalchemy import Column, Integer, String, Enum
+import enum
+import uuid
 
-from ..db import Base
+from sqlalchemy import Column, Integer, String, Enum, DateTime, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
+from src.db import Base
 
 
-class UserModel(Base):
-    __tablename__ = "users"
+class SocialMediaType(enum.Enum):
+    vk = "VK"
+    fb = "Facebook"
+    inst = "Instagram"
+    tg = "Telegram"
+    tw = "Twitter"
+    wa = "WhatsApp"
+    vb = "Viber"
 
-    id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String, unique=True, index=True)
-    last_name = Column(String)
+
+class Roles(enum.Enum):
+    photographer = "Photographer"
+    customer = "Customer"
+    admin = "Admin"
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(UUID, default=uuid.uuid4(), primary_key=True, index=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
     middle_name = Column(String)
-    age = Column(Integer)
-    email = Column(String)
-    phone = Column(Integer)
-    city = Column(Enum)
+    password = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True, index=True)
+    phone = Column(String)
+    experience = Column(Integer)
+    city = Column(String)
+    role = Column(Enum(Roles), default=Roles.customer, nullable=False, index=True)
+    birthdate = Column(DateTime, nullable=False)
+    about = Column(String)
+
+    social_media = relationship("SocialMedia", cascade="all, delete")
+    # portfolio = fore
+    # genres = fore
+    # orders = fore
+
+
+class SocialMedia(Base):
+    __tablename__ = "social media"
+
+    id = Column(UUID, default=uuid.uuid4(), primary_key=True, index=True)
+    user_id = Column(UUID, ForeignKey("user.id"), nullable=False)
+    link = Column(String, nullable=False)
+    type = Column(Enum(SocialMediaType), nullable=False)
 
--- src/user_service/schemas.py
+++ src/user_service/schemas.py
@@ -1,10 +1,40 @@
+from datetime import datetime
+import random
+from typing import Optional
+from uuid import UUID
+
 from pydantic import BaseModel
 
+from src.user_service.models import Roles
 
-class UserSchema(BaseModel):
+
+def generate_pwd() -> str:
+    symbols_list = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    password = ""
+    length = random.randint(6, 10)
+    for i in range(length):
+        password += random.choice(symbols_list)
+    return password
+
+
+class UserBase(BaseModel):
     first_name: str
-    last_name: str = None
-    age: int
+    last_name: str
+    middle_name: Optional[str] = None
+    email: str
+    phone: Optional[str] = None
+    birthdate: Optional[datetime] = None
+    city: Optional[str] = None
+    role: Roles = Roles.customer
 
     class Config:
         orm_mode = True
+
+
+class UserCreate(UserBase):
+    password: str = generate_pwd()
+
+
+class UserDB(UserBase):
+    id: UUID
+    password: str
